dcd51e1b6802fbcae2ae974fca7aea65df304171
Christian Bradley <chris@chrisgbradley.com>
2022-07-06 01:58:37 -0400

    change about page description

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -11,7 +11,7 @@
 
   // since there's no dynamic data here, we can prerender
   // it so that it gets served as a static asset in prod
-  // export const prerender = true;
+  export const prerender = true;
 </script>
 
 <svelte:head>
@@ -26,32 +26,48 @@
     <h3 class="text-2xl font-semibold pt-8 lg:max-w-3xl leading-loose">A personal challenge to encourage rapid <span class="text-orange font-mono">prototyping</span>, better <span class="text-orange font-mono">UI design</span> skills, and practice <span class="text-orange font-mono">developing experiences</span>.</h3>
     <h4 class="font-bold text-xl pt-20">The Longer Version</h4>
     <p class="text-lg font-normal pt-8 lg:max-w-4xl leading-loose">
-      I decided to take the 100 Days of Design from <a class="link text-orange underline" href="https://www.dailyui.co">DailyUI</a> and push it one step further. Rotating through 10 popular industries, each day a design is crafted to spec for the given prompt, then converted to HTML, CSS, & JS and uploaded as a page here. All pages should act independently of each other and should be fully functioning up until an actual payment or redirection is requested. All logos will simply reload page. All links will reload the page, unless there is a useful link that can be established. 
+      I decided to take the 100 Days of Design from <a class="link " href="https://www.dailyui.co">DailyUI</a> and push it one step further. Rotating through 10 popular industries, each day a design is crafted to spec for the given prompt, developed using Svelte, and hosted -- currently on Vercel.
     </p>
+    <h5 class="font-bold text-lg pt-8">The Rules</h5>
+    <ul class="text-md leading-relaxed list-inside list-decimal pl-6">
+      <li>All pages are independent.
+        <ul class="list-inside list-disc pl-6"><li>This means separate CSS files. Repitition is half of the point of this project.</li></ul>
+      </li>
+      <li>All tools are on the table. 
+        <ul class="list-inside list-disc pl-6">
+          <li>Render however, host whereever, custom or prebuilt components...</li>
+          <li>As long as Svelte is used and the design matches the concept, go nuts.</li>
+        </ul>
+      </li>
+      <li>All pages should be fully responsive.</li>
+      <li>All pages should be fully accessible.</li>
+    </ul>
+
     <h4 class="font-bold text-xl pt-20">The Why</h4>
     <p class="text-lg font-normal pt-8 lg:max-w-4xl leading-loose">
-      My biggest goal from all of this is to become a better designer and developer. I would like to get better at shooting for perfection <span class="text-orange font-semibold">inside of a deadline</span>. Additionally, one of the greatest issues I have had in the past 8 years is finding inspiration for projects. I'm always either consistently too busy with family, school, or work or I scope far to large for a project and didn't have the passion for the project to start with.
+      This project allows practice with rapid prototyping, improves knowledge of a framework and its community, and provides training wheels for taming the scope monster by applying a faux deadline. In addition to the deadline, the page is already designed, so having the end product already in mind enforces the 'good enough' mentality and encourages you to move on and continue learning.
     </p>
+
     <h4 class="font-bold text-xl pt-20">The Resources</h4>
-    <div class="flex flex-row align-top">
-      <div>
-        <h5 class="font-semibold text-lg py-6 pr-16">Inspiration & Learning</h5>
-        <ul class="text-md leading-relaxed">
-          <li>DailyUI</li>
-          <li>Awwwards</li>
-          <li>LevelUpTuts</li>
-          <li>DesignCourse</li>
-          <li>Kevin Powell</li>
+    <div class="flex flex-row pl-6 basis-full flex-1 lg:max-w-4xl">
+      <div class="flex flex-col flex-1 basis-full">
+        <h5 class="font-semibold text-lg pt-6">Inspiration & Learning</h5>
+        <ul class="text-md leading-relaxes pl-6">
+          <li><a target="_blank" rel="noopener noreferrer" href="https://www.dailyui.co" class="link">DailyUI</a></li>
+          <li><a target="_blank" rel="noopener noreferrer" href="https://www.awwwards.com" class="link">Awwwards</a></li>
+          <li><a target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/c/LevelUpTuts" class="link">LevelUpTuts</a></li>
+          <li><a target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/c/DesignCourse" class="link">DesignCourse</a></li>
+          <li><a target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/c/kepowob" class="link">Kevin Powell</a></li>
         </ul>
       </div>
 
-      <div>
-        <h5 class="font-semibold text-lg py-6">Tools</h5>
-        <ul class="text-md leading-relaxed">
-          <li>Figma</li>
-          <li>Svelte</li>
-          <li>TailwindCSS</li>
-          <li>Vercel</li>
+      <div class="flex flex-col flex-1 basis-full">
+        <h5 class="font-semibold text-lg pt-6">Tools</h5>
+        <ul class="text-md leading-relaxes pl-6">
+          <li><a target="_blank" rel="noopener noreferrer" href="https://www.figma.com" class="link">Figma</a></li>
+          <li><a target="_blank" rel="noopener noreferrer" href="https://svelte.dev" class="link">Svelte</a> / <a target="_blank" rel="noopener noreferrer" href="https://kit.svelte.dev" class="link">SvelteKit</a></li>
+          <li><a target="_blank" rel="noopener noreferrer" href="https://tailwindcss.com" class="link">TailwindCSS</a></li>
+          <li><a target="_blank" rel="noopener noreferrer" href="https://vercel.com/" class="link">Vercel</a></li>
         </ul>
       </div>
     </div>
@@ -70,7 +86,7 @@
   .link {
     display: inline-block;
     position: relative;
-    padding: 0;
+    padding: 12px 0 0 0;
     text-decoration: none;
     transition: all ease-in 150ms;
   }
@@ -78,7 +94,7 @@
   .link::after {
     content: '';
     position: absolute;
-    bottom: 0;
+    bottom: 10%;
     left: 0;
     width: 100%;
     height: 0.1em;
@@ -92,5 +108,7 @@
 
   .link:hover::after {
     background-color:#D2D0CF;
+    height: 0.15em;
+    bottom: 5%;
   }
 </style>
