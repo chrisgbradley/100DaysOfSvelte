6f6bd4a9ddd5b65dbdfdfcfa45fab799e4c7e0b5
Christian Bradley <chris@chrisgbradley.com>
2022-07-12 19:28:24 -0400

    meta viewport fix for mobile

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -33,6 +33,7 @@ import ThemeProvider from '$cmp/theme/ThemeProvider.svelte';
 
 
 <svelte:head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin=true>
   <link href="https://fonts.googleapis.com/css2?family=Overpass+Mono:wght@400;700&family=Overpass:wght@400;600;900&family=Pacifico&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
