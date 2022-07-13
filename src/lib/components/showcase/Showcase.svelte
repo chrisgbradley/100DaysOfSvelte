1e87a1447a6032bf1b0ec24709a0871361d628e8
Christian Bradley <chris@chrisgbradley.com>
2022-07-13 14:41:40 -0400

    remove old card components

--- src/lib/components/showcase/Showcase.svelte
+++ src/lib/components/showcase/Showcase.svelte
@@ -1,16 +1,9 @@
 <script lang="ts">
   import { flip } from 'svelte/animate';
-  import WeekCard from './WeekCard.svelte';
   import { weeks } from '$lib/designs.json';
-  import Index from '$src/routes/index.svelte';
   import Card from './Card.svelte';
 
-  // cards: []
-  // -- index: number
-  // -- type: Types.Week | Types.Design
-  // -- show: boolean
-
-  // cards = weeks.map()
+  
   let cards = weeks.flatMap((week) => {
     let wIdx = week.index;
     let designs = week.designs.map(({ index, ...obj }) => ({
@@ -50,17 +43,6 @@
     </div>
   {/each}
 </div>
-
-<!-- <div
-  class="text-accent border-primary border-solid border-2 flex flex-col items-center justify-center gap-16 bg-secondary bg-opacity-10 h-[200px] w-1/2 sm:w-1/3 lg:h-[250px] xl:w-1/6 xl:h-[175px] 2xl:h-[200px] md:rounded hover:text-secondary hover:bg-accent hover:bg-opacity-50 transition-all ease-in-out duration-150"
-/>
-
-<div
-  class="border-primary border-solid border-2 flex items-center justify-center h-[200px] w-1/2 sm:w-1/3 lg:h-[250px] xl:w-1/6 xl:h-[175px] 2xl:h-[200px] md:rounded"
-  on:click={() => {
-    week.show = !week.show;
-  }}
-/> -->
 
 
 <style>
